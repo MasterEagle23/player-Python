58bed0954d43eec658e9f57d4adefd78b3c87454
Tsunamie <151086637+Tsunamie1337@users.noreply.github.com>
2024-05-18 00:35:54 +0200

    added attack check

---
+++ logic/attack_alert.py
@@ -0,0 +1,13 @@
+from models.base import Base
+from models.base_level import BaseLevel
+from models.game_state import GameState
+from strategy import *
+
+def attack_checker(base: Base):
+    bases = get_base_list()
+    attacked_at=[]
+    for i in range(len(gamestate.actions)):
+        for j in range(len(bases[0])):
+            if gamestate.actions[i].dest == bases[0][j]:
+                attacked_at.append(j)
+    return attacked_at
