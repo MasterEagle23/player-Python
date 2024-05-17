7f59f96edb14ff92227a803ca73a6af8c6213563
konrad_john@t-online.de <noLr^]h!%695Z1pB9I[N>
2024-05-18 01:41:13 +0200

    fix

--- logic/me_functions.py
+++ logic/me_functions.py
@@ -45,4 +45,4 @@ def get_grace_period() -> int:
     return gamestate.config.paths.grace_period
 
 def get_death_rate() -> int:
-    return gamestate.config.paths.
+    return gamestate.config.paths.death_rate
